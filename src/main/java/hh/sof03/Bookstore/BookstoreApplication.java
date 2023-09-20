61a2d50293b1fb002b95849ec82b6ed0c87aec56
jonitirk <jonitirkkonenmusic@gmail.com>
2023-09-20 17:45:02 +0300

    lisätty tietokanta ja crud ominaisuudet

--- src/main/java/hh/sof03/Bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof03/Bookstore/BookstoreApplication.java
@@ -1,13 +1,26 @@
 package hh.sof03.Bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh.sof03.Bookstore.domain.Book;
+import hh.sof03.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {return (args) -> {
+	 Book test = new Book("Harry Potter", "J.K.Rowling", "123-46-8", 2001, 5.00);
+	 
+	 repository.save(test);
+	};
 	}
 
 }
--- src/main/java/hh/sof03/Bookstore/domain/Book.java
+++ src/main/java/hh/sof03/Bookstore/domain/Book.java
@@ -1,7 +1,15 @@
 package hh.sof03.Bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
 	private String title, author, isbn;
